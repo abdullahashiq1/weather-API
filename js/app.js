fb54ec578225b57b31438a2e686f467e57480688
abdullahashiq1 <ashdev491@gmail.com>
2023-09-27 23:15:37 +0600

    modal add

--- js/app.js
+++ js/app.js
@@ -1,52 +1,49 @@
 let cardLimit = 6;  // initialy card limit
 
-const loadWeatherAPI = async( cardLimit)=>{
+const loadAIAPI = async( cardLimit)=>{
     const url = `https://openapi.programming-hero.com/api/ai/tools`;
     const res = await fetch(url);
     const data =await res.json()
-    displayWeather(data.data.tools, cardLimit)
+    displayAI(data.data.tools, cardLimit)
 }
 
-const displayWeather = (weathers, cardLimit) =>{
+const displayAI = (allAPIs, cardLimit) =>{
     const container = document.querySelector('#container');
 
     // only show six cards on UI 
-    // weathers = weathers.slice(0, 6); 
+    // allAPIs = allAPIs.slice(0, 6); 
     const showAll = document.getElementById('show-all');
-    if(weathers.length> cardLimit){
-        weathers = weathers.slice(0, cardLimit); 
+    if(allAPIs.length> cardLimit){
+        allAPIs = allAPIs.slice(0, cardLimit); 
         showAll.classList.remove('hidden')
     }else{
         showAll.classList.add('hidden')
     }
 
-    weathers.forEach((weather)=>{
-        // console.log(weather)
+    allAPIs.forEach((api)=>{
+        // console.log(api)
         
         const div = document.createElement('div');
         div.classList.add('show-card');
         div.innerHTML = `
             <div class="max-w-sm rounded overflow-hidden shadow-lg p-3 ">
-                <img class="w-full rounded" src='${weather?.image}' alt="Sunset in the mountains">
+                <img class="w-full rounded" src='${api?.image}' alt="Sunset in the mountains">
                 <div class="px-6 py-4">
                     <div class="font-bold text-xl mb-2">Features</div>
-                    <p class="text-gray-700 text-base">1. ${weather.features[0]}</p>
-                    <p class="text-gray-700 text-base">2. ${weather.features[1]}</p>
-                    <p class="text-gray-700 text-base">3. ${weather.features[2]}</p>
+                    <p class="text-gray-700 text-base">1. ${api.features[0]}</p>
+                    <p class="text-gray-700 text-base">2. ${api.features[1]}</p>
+                    <p class="text-gray-700 text-base">3. ${api.features[2]}</p>
                 </div>
                 <hr>
                 <div class="px-6 pt-4 pb-2">
-                    <h3 class="font-bold text-2xl mb-3">${weather.name}</h3>
+                    <h3 class="font-bold text-2xl mb-3">${api.name}</h3>
                     <span><i style="color: black;" class="fa-regular fa-calendar-days"></i>
-                        ${weather.published_in}
+                        ${api.published_in}
+                    </span>
 
-                        <button onclick="loadDetails('${weather.id}')" class="float-right text-red-700 bg-white border-2 border-rose-300 focus:outline-none hover:bg-gray-100 focus:ring-4 
-                        focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 
-                        dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 
-                        dark:focus:ring-gray-700" type="button">
-                            <i class="fa-solid fa-arrow-right"></i>
-                        </button>
-                        </span>
+                    <button onclick="detailsAI.showModal(); loadDetails('${api.id}')" class="btn float-right text-red-700 bg-white border-2 border-rose-300 px-5 py-2"  type="button">
+                        <i class="fa-solid fa-arrow-right"></i>
+                    </button>
 
                 </div>
             </div>
@@ -59,7 +56,7 @@ const displayWeather = (weathers, cardLimit) =>{
 }
 
 const showCard = (cardLimit)=>{
-    loadWeatherAPI(cardLimit);
+    loadAIAPI(cardLimit);
     // start spinner 
     spinner(true) 
 }
@@ -86,15 +83,25 @@ const spinner = isLoading =>{
 
 // load details API 
 const loadDetails =async id =>{
-    console.log('Yes!', id)
     const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`
     const res = await fetch(url)
     const data = await res.json()
-    console.log(data);
+    displayDetail(data.data);
 
 }
 
-loadWeatherAPI(cardLimit);  // initially load six cards
+const displayDetail = (detail)=>{
+    console.log(detail)
+    // 
+    const modalLeft = document.getElementById('modal-left');
+    modalLeft.innerHTML = `
+        <h3 class="font-bold text-lg">${detail.description}</h3>
+        
+    `
+}
+
+
+loadAIAPI(cardLimit);  // initially load six cards
 
 
 
