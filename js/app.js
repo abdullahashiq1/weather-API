dcc3acb4187d12a19b544efa1a5d7fc9220ddf7a
abdullahashiq1 <ashdev491@gmail.com>
2023-09-27 19:42:58 +0600

    Show all Cards

--- js/app.js
+++ js/app.js
@@ -1,18 +1,30 @@
-const loadWeatherAPI = async()=>{
+let cardLimit = 6;  // initialy card limit
+
+const loadWeatherAPI = async(cardLimit)=>{
     const url = `https://openapi.programming-hero.com/api/ai/tools`;
     const res = await fetch(url);
     const data =await res.json()
-    displayWeather(data.data.tools)
+    displayWeather(data.data.tools, cardLimit)
 }
 
-const displayWeather = (weathers) =>{
-    // console.log(weathers)
+const displayWeather = (weathers, cardLimit) =>{
     const container = document.querySelector('#container');
-    weathers.forEach(weather =>{
+
+    // only show six cards on UI 
+    // weathers = weathers.slice(0, 6); 
+    const showAll = document.getElementById('show-all');
+    if(weathers.length> cardLimit){
+        weathers = weathers.slice(0, cardLimit); 
+        showAll.classList.remove('hidden')
+    }else{
+        showAll.classList.add('hidden')
+    }
+
+    weathers.forEach((weather)=>{
         console.log(weather)
-        console.log();
+        
         const div = document.createElement('div');
-        div.classList.add('my-class')
+        div.classList.add('show-card');
         div.innerHTML = `
             <div class="max-w-sm rounded overflow-hidden shadow-lg p-3 ">
                 <img class="w-full rounded" src='${weather?.image}' alt="Sunset in the mountains">
@@ -27,7 +39,7 @@ const displayWeather = (weathers) =>{
                     <h3 class="font-bold text-2xl mb-3">${weather.name}</h3>
                     <span><i style="color: black;" class="fa-regular fa-calendar-days"></i>
                         ${weather.published_in}
-                        
+
                         <button class="float-right text-red-700 bg-white border-2 border-rose-300 focus:outline-none hover:bg-gray-100 focus:ring-4 
                         focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 
                         dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 
@@ -40,9 +52,25 @@ const displayWeather = (weathers) =>{
             </div>
         `
         container.appendChild(div)
-
-
-    })
+       
+    });
+    
 }
 
-loadWeatherAPI()
+const showCard = (cardLimit)=>{
+    loadWeatherAPI(cardLimit)
+}
+
+document.getElementById('show-all').addEventListener('click', function(){
+    if(cardLimit === 6){
+        cardLimit = Infinity;   // show all cards
+    }else{
+        cardLimit = 6   // show six cards
+    }
+    showCard(cardLimit)  
+})
+
+loadWeatherAPI(cardLimit);  // initially load six cards
+
+
+
