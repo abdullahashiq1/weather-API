1fc4f203741aec8ce20a67672e90979f1b1e3b3f
abdullahashiq1 <ashdev491@gmail.com>
2023-09-27 22:24:20 +0600

    spinner && single API call

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 let cardLimit = 6;  // initialy card limit
 
-const loadWeatherAPI = async(cardLimit)=>{
+const loadWeatherAPI = async( cardLimit)=>{
     const url = `https://openapi.programming-hero.com/api/ai/tools`;
     const res = await fetch(url);
     const data =await res.json()
@@ -21,7 +21,7 @@ const displayWeather = (weathers, cardLimit) =>{
     }
 
     weathers.forEach((weather)=>{
-        console.log(weather)
+        // console.log(weather)
         
         const div = document.createElement('div');
         div.classList.add('show-card');
@@ -40,7 +40,7 @@ const displayWeather = (weathers, cardLimit) =>{
                     <span><i style="color: black;" class="fa-regular fa-calendar-days"></i>
                         ${weather.published_in}
 
-                        <button class="float-right text-red-700 bg-white border-2 border-rose-300 focus:outline-none hover:bg-gray-100 focus:ring-4 
+                        <button onclick="loadDetails('${weather.id}')" class="float-right text-red-700 bg-white border-2 border-rose-300 focus:outline-none hover:bg-gray-100 focus:ring-4 
                         focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 
                         dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 
                         dark:focus:ring-gray-700" type="button">
@@ -54,11 +54,14 @@ const displayWeather = (weathers, cardLimit) =>{
         container.appendChild(div)
        
     });
-    
+    // stop spinner 
+    spinner(false);   
 }
 
 const showCard = (cardLimit)=>{
-    loadWeatherAPI(cardLimit)
+    loadWeatherAPI(cardLimit);
+    // start spinner 
+    spinner(true) 
 }
 
 document.getElementById('show-all').addEventListener('click', function(){
@@ -67,8 +70,29 @@ document.getElementById('show-all').addEventListener('click', function(){
     }else{
         cardLimit = 6   // show six cards
     }
-    showCard(cardLimit)  
+    showCard(cardLimit) 
+    
 })
+
+// loader 
+const spinner = isLoading =>{
+    const loader = document.getElementById('loader');
+    if(isLoading){
+        loader.classList.remove('hidden')
+    }else{
+        loader.classList.add('hidden');
+    }
+}
+
+// load details API 
+const loadDetails =async id =>{
+    console.log('Yes!', id)
+    const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`
+    const res = await fetch(url)
+    const data = await res.json()
+    console.log(data);
+
+}
 
 loadWeatherAPI(cardLimit);  // initially load six cards
 
